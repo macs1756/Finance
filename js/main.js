092cdb0897e1224c4f9d9f0b08633d1cfc9f51e4
macs1756 <kindrativmaksum312@gmail.com>
2022-06-19 14:14:20 +0200

    list animation

--- js/main.js
+++ js/main.js
@@ -7,3 +7,36 @@ btnBurger.addEventListener("click", item =>{
 		btnBurger.classList.toggle("header__burger_close");
 		contentBurger.classList.toggle("burger__open");
 });
+
+
+//initial list 
+
+
+
+
+const initialList = document.querySelector(".search__bth-list");
+const initialBtn = document.querySelector("#initialBtn");
+
+initialBtn.addEventListener("click", click=>{
+	initialList.classList.toggle("activeList");
+	initialBtn.classList.toggle("btnBorder");
+});
+
+initialList.addEventListener("click", listClick=>{
+	initialList.classList.remove("activeList");
+	initialBtn.classList.remove("btnBorder");
+});
+
+//btn content
+
+const listItem = document.querySelectorAll(".search__bth-list-item");
+
+
+listItem.forEach(content=>{
+	content.addEventListener("click", clickContent=>{
+		initialBtn.innerHTML = content.innerHTML;
+
+	});
+});
+
+
